a518bb3e89c4bfe7f6656cbea3a7b3b7195f2b98
Mariam Fatima <mariam37009@gmail.com>
2026-06-25 02:53:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MariamF35.github.io/',
-  plugins: [react(), tailwindcss()],
+  base: './',
+  plugins: [react(),],
 });
